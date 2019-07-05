a8d506efed341d9bb8da72e7a87ad0b32f17ea47
Sangavianandh <sangavi.anandhasamy@aspiresys.com>
2019-07-05 15:27:12 +0530

    Perl Program

---
+++ july1/Project.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/perl
+use MIME::Lite;
+use DBI;
+use strict;
+use warnings;
+use Email::Valid;
+require login;
+require register;
+my $sth;
+my $driver = "mysql"; 
+my $database = "messageApp";
+my $hostname="localhost";
+my $dsn = "DBI:$driver:database=$database:host=$hostname";
+my $userid = "root";
+my $password = "mysql";
+my $dbh = DBI->connect($dsn, $userid, $password ) or die $DBI::errstr;
+my ($email,$mobile,$input,$subject,$message,$msg,$choice,$object);
+
+
+print "Enter your choice\n 1.Login\n 2.Register\n";
+$input=<STDIN>;
+chomp($input);
+
+#Login
+
+  if($input==1)
+   {
+     print "login your account\n";
+     print "Enter the emailId\n";
+     $email=<STDIN>;
+     chomp($email);
+     print "Enter the Mobile Number\n";
+     $mobile=<STDIN>;
+     chomp($mobile);
+ $object=new login($email,$mobile);
+  }
+   
+#Register
+else
+{
+      label:
+       print "Register the account\n";
+#email 
+       print "Enter the email id\n";
+       $email=<STDIN>;
+        chomp($email);
+#mobile Number
+        print "Enter the mobile number\n";
+        $mobile=<STDIN>;
+        chomp($mobile);
+        $subject = 'Test Email';
+        $message = '<p>Hi all, <\br> Greetings. <\br> welcome to aspire systems<p> ';
+
+#validating the email address
+
+        unless( Email::Valid->address($email) )
+         {
+         print "Sorry, from email address is not valid!";
+         goto label;
+         }
+ $object=new register($email,$mobile);
+        $sth=$dbh->prepare("insert into register(mobile,email) values(?,?)");
+        $sth->execute($mobile,$email);
+}
+
+#for sending email
+
+        $sth=$dbh->prepare("SELECT email,mobile  FROM register where email=? ");
+        $sth->execute($email);
+        if ($dbh->selectrow_array($sth, undef, $email))
+{
+          print "this mail is exist in database";
+          $msg = MIME::Lite->new(
+                 to       => $email,
+                 Subject  => $subject,
+                 Type     => 'multipart/mixed'
+                 );
+# Add your text message.
+          $msg->attr(Type         => 'text/html',
+             Data         => $message
+             );
+        print "Enter if you want to attach the image\n";
+        $choice=<STDIN>;
+        chomp($choice);
+
+# Specify your file as attachement.
+       if($choice eq "yes"||"YES")
+      {
+            $msg->attach(Type         => 'image/gif',
+                 Path         => '/home/adpc-56/PerlRepository/Natural.jpg',
+                   Filename     => 'Natural.jpg',
+                Disposition  => 'attachment'
+                ); 
+       }    
+      $msg->send;
+      print "Email Sent Successfully\n";
+
+}
+else
+{
+print "You need to register";
+goto label;
+}
+
---
+++ july1/login.pm
@@ -0,0 +1,38 @@
+package login;e
+
+sub new {
+   my $class = shift;
+   my $self = {
+      _email => shift,
+      _mobile  => shift,
+     
+   };
+   # Print all the values just for clarification.
+   print "email is $self->{_email}\n";
+   print "mobile number is $self->{_mobile}\n";
+   bless $self, $class;
+   return $self;
+}
+sub setEmail {
+   my ( $self, $email ) = @_;
+   $self->{_email} = $email if defined($email);
+   return $self->{_email};
+}
+
+sub getEmail {
+   my( $self ) = @_;
+   return $self->{_email};
+}
+sub setMobile{
+my ($self,$mobile)=@_;
+$self->{_mobile}=$mobile if defined($mobile);
+return $self->{_mobile};
+}
+
+sub getMobile {
+   my( $self ) = @_;
+   return $self->{_mobile};
+}
+1;
+
+
